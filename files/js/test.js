e9d6dfe5510de12aff4286c98a17384420f92299
syo2819 <83334688+syo2819@users.noreply.github.com>
2023-09-25 18:02:15 +0900

    Update test.js

--- files/js/test.js
+++ files/js/test.js
@@ -13,7 +13,16 @@ $(function() {
   }
 
   $("#崩壊").click(function(){
-    alert("無事")
+    if ($("#input").val() == "") {
+			alert("何か入力して下さい");
+			$("#input").focus();
+			return;
+		}
+		if ($("#input").val().length > 200) {
+			alert("文字列は200文字以内で入力して下さい");
+			$("#input").focus();
+			return;
+		}
     let ur = "https://script.google.com/macros/s/AKfycbyTYTcTLSFTg7PzjgWDPG7MTafvNR3pI_gcjDd6QPYbR7-iDBt4iMEyjoZNhQCEbNwiCw/exec?text=" + $("#input").val();
     tra.open("GET", ur);
     tra.send(null);
